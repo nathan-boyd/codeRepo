8130a7ae80879a7a47eb133a883a29635a8fcc20
Nathan Boyd <nathan@nathan-boyd.org>
2012-02-13 22:26:34 -0500

    CS 403 MP1 Everything Works
    Signed-off-by: Nathan Boyd <nathan@nathan-boyd.org>

--- live_code/CS403/mp1given/threads/Database.java
+++ live_code/CS403/mp1given/threads/Database.java
@@ -6,22 +6,22 @@ public class Database
    //MP1 create any variables that you need for implementation of the methods
    //of this class
 
-    private int readerCount; 	
-    Semaphore mutex;
-    Semaphore db;
+    private int readerCount, writerCount; 	
+    private Semaphore alpha, beta, gamma, delta, epsilon;
     
-
    //Database
    //Initializes Database variables
    public Database()
    {
-     //MP1
        
        readerCount 	= 0;
-       mutex 		= new Semaphore("mutex", 1);
-       db 		= new Semaphore("db", 1);
+       alpha 		= new Semaphore("alpha", 	1);
+       beta 		= new Semaphore("beta", 	1);
+       gamma 		= new Semaphore("gamma", 	1);
+       delta 		= new Semaphore("delta", 	1);
+       epsilon 		= new Semaphore("epsilon", 	1);
        
-   }
+   }//end constructor
 
    //napping()
    //this is called when a reader or writer wants to go to sleep and when 
@@ -30,7 +30,7 @@ public class Database
    public static void napping()
    {
       Alarm ac = new Alarm(20);  
-   }
+   }//end function
 
    //startRead
    //this function should block any reader that wants to read if there 
@@ -40,20 +40,22 @@ public class Database
    public int startRead()
    {
        //MP1
-       
-       mutex.P();
-       ++readerCount;
-       
-       // if I am the first reader tell all others that the database is being read
-       if (readerCount == 1) {
-	   db.V();
-       }
-       
-       mutex.V();
+        epsilon.P();
+        alpha.P();
+        gamma.P();
+        readerCount++;
+    
+        if (readerCount==1){
+            beta.P();
+        }//end if
+        
+        gamma.V();
+        alpha.V();
+        epsilon.V();
+        
+        return readerCount;
 
-       return readerCount;       
-       
-   }
+   }//end function
 
    //endRead()
    //This function is called by a reader that has finished reading from the 
@@ -63,19 +65,18 @@ public class Database
    {
       //MP1
        
-       mutex.V();
-       --readerCount;
-       
-       // if I am the last reader tell all others that the database is no longer being read
-       if (readerCount == 0) {
-	   db.P();   
-       }
-       
-       mutex.P();
-       
-       return readerCount;
-       
-   }
+        gamma.P();
+        readerCount--;
+        
+        if (readerCount==0) {
+            beta.V();
+        }//end if
+        
+        gamma.V();
+        
+        return readerCount;
+
+   }//end function
 
    //startWrite()
    //This function should allow only one writer at a time into the Database
@@ -84,17 +85,37 @@ public class Database
    public void startWrite()
    {
       //MP1
-       db.V();
+       
+        delta.P();
+        writerCount++;
 
-   }
+        if (writerCount==1) {
+            alpha.P();
+        }//end if
+        
+        delta.V();
+        
+        beta.P();
+               
+   }//end function
    
    //endWrite()
    //signal that a writer is done writing and the database can now be accessed
    //by someone who is waiting to read or write.
    public void endWrite()
    {
-      //MP1
-       db.P();
+      //MP1       
+
+        beta.V();
+        delta.P();
+        writerCount--;
+        
+        if (writerCount==0) {
+            alpha.V();
+        }//end if
+        
+        delta.V();
        
-   }
-}
+   }//end function
+
+}//end class
